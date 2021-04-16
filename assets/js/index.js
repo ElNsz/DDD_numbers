aa75f8b507c5b78b47294dc35a7730185c8b6a72
Ellivelton Narciso <ellivelton.souza@gmail.com>
2021-04-16 15:00:53 -0300

    Tirando o excesso de <li> e aproveitando mais do que o Vue tem a oferecer.

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,96 +13,31 @@ let app = new Vue({
 let listarNum = new Vue({
     el: '.listaNum',
     data:{
-        alert: 'Por favor, preencha no mínimo 8 e no máximo 9 digitos.'
+        alert: 'Por favor, preencha no mínimo 8 e no máximo 9 digitos.',
+        lista: [{ddd:'61'},{ddd:'62'},{ddd:'64'},{ddd:'65'},{ddd:'66'},{ddd:'67'},{ddd:'82'},{ddd:'71'},{ddd:'73'},{ddd:'74'},{ddd:'75'},{ddd:'77'},{ddd:'85'},{ddd:'88'},{ddd:'98'},{ddd:'99'},{ddd:'83'},{ddd:'81'},{ddd:'87'},{ddd:'86'},{ddd:'89'},{ddd:'84'},{ddd:'79'},{ddd:'68'},{ddd:'96'},{ddd:'92'},{ddd:'97'},{ddd:'91'},{ddd:'93'},{ddd:'94'},{ddd:'69'},{ddd:'95'},{ddd:'63'},{ddd:'27'},{ddd:'28'},{ddd:'31'},{ddd:'32'},{ddd:'33'},{ddd:'34'},{ddd:'35'},{ddd:'37'},{ddd:'38'},{ddd:'21'},{ddd:'22'},{ddd:'24'},{ddd:'11'},{ddd:'12'},{ddd:'13'},{ddd:'14'},{ddd:'15'},{ddd:'16'},{ddd:'17'},{ddd:'18'},{ddd:'19'},{ddd:'41'},{ddd:'42'},{ddd:'43'},{ddd:'44'},{ddd:'45'},{ddd:'46'},{ddd:'51'},{ddd:'53'},{ddd:'54'},{ddd:'55'},{ddd:'47'},{ddd:'48'},{ddd:'49'}],
+        mostrar:false,
+        numeros:'',
+        link:''
     },
     methods: {
-        enviar: function enviar() {
+        enviar() {
             if (qs('.input_num').value.length < 8) {
                 qs('.alert').style.display = 'block'
                 qs('.input_num').focus();
+                listarNum.mostrar = false
                 return false
             }
             if (qs('.input_num').value.length > 9) {
                 qs('.alert').style.display = 'block'
-                qs('.alert').style.display = 'block'
                 qs('.input_num').focus();
+                listarNum.mostrar = false
                 return false
             }
             else {
 
-                var valor = qs('.input_num').value; // Pegar o valor do input
-                function qsih(order, number) {
-                    qs('.alert').removeAttribute('style')
-                    qs('.' + order).innerHTML = 'https://wa.me/+55' + number + valor
-                    qs('.' + order).href = 'https://wa.me/+55' + number + valor
-                }
-                qsih('a', '61')
-                qsih('b', '62')
-                qsih('c', '64')
-                qsih('d', '65')
-                qsih('e', '66')
-                qsih('f', '67')
-                qsih('g', '82')
-                qsih('h', '71')
-                qsih('i', '73')
-                qsih('j', '74')
-                qsih('k', '75')
-                qsih('l', '77')
-                qsih('m', '85')
-                qsih('n', '88')
-                qsih('o', '98')
-                qsih('p', '99')
-                qsih('q', '83')
-                qsih('r', '81')
-                qsih('s', '87')
-                qsih('t', '86')
-                qsih('u', '89')
-                qsih('v', '84')
-                qsih('w', '79')
-                qsih('x', '68')
-                qsih('y', '96')
-                qsih('z', '92')
-                qsih('aa', '97')
-                qsih('ab', '91')
-                qsih('ac', '93')
-                qsih('ad', '94')
-                qsih('ae', '69')
-                qsih('af', '95')
-                qsih('ag', '63')
-                qsih('ah', '27')
-                qsih('ai', '28')
-                qsih('aj', '31')
-                qsih('ak', '32')
-                qsih('al', '33')
-                qsih('am', '34')
-                qsih('an', '35')
-                qsih('ao', '37')
-                qsih('ap', '38')
-                qsih('aq', '21')
-                qsih('ar', '22')
-                qsih('as', '24')
-                qsih('at', '11')
-                qsih('au', '12')
-                qsih('av', '13')
-                qsih('aw', '14')
-                qsih('ax', '15')
-                qsih('ay', '16')
-                qsih('az', '17')
-                qsih('ba', '18')
-                qsih('bb', '19')
-                qsih('ba', '41')
-                qsih('bb', '42')
-                qsih('ba', '43')
-                qsih('bb', '44')
-                qsih('ba', '45')
-                qsih('bb', '46')
-                qsih('ba', '51')
-                qsih('bb', '53')
-                qsih('ba', '54')
-                qsih('bb', '55')
-                qsih('ba', '47')
-                qsih('bb', '48')
-                qsih('bc', '49')
+                let valor = qs('.input_num').value; // Pegar o valor do input
+                listarNum.mostrar = true
+                listarNum.numeros = valor
 
             }
 
